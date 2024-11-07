aacfd3ff02944f5ed9ca81855b118cd42542aecb
Alejandro Alejos <ikdvc@plaiaundi.net>
2024-11-07 10:05:52 +0100

    commit holaMadeline

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola alejandro!");
+        System.out.println("Hola Madeline!");
     }
 }
